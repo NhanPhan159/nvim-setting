07e74df8670c13c5c6076a62689a703949ba003f
NhanPhan159 <nhandut2019@gmail.com>
2026-04-10 10:04:07 +0700

    add keymap for actions with nvim-tree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 local opts = { noremap = true, silent = true }
 
 local term_opts = { silent = true }
+local apiNvimTree = require("nvim-tree.api")
 
 -- Shorten function name
 local keymap_api = vim.api.nvim_set_keymap
@@ -12,6 +13,18 @@ keymap_api("n", "<S-n>", "<C-w>l", opts)
 keymap_api("n", "<S-h>", "<C-w>h", opts)
 keymap_api("n", "<S-j>", "<C-w>j", opts)
 keymap_api("n", "<S-k>", "<C-w>k", opts)
+
+
+-- File explore
+keymap_set("n", "<C-c>", function()
+  apiNvimTree.tree.close()
+end, { desc = "Open file explore" }, opts)
+
+keymap_set("n", "<C-o>", function()
+  apiNvimTree.tree.open()
+end, { desc = "Open file explore" }, opts)
+
+
 
 -- Action with code
 keymap_set("n", "K", function()
